2dcc9755856898e6cccd7f61363e1eaa46679445
Anya Lehman <lehmana@oregonstate.edu>
2017-02-02 15:33:20 -0800

    Merge https://github.com/OSU-CS361-W17/project1
    # Conflicts:
    #	src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
    #	src/main/java/edu/oregonstate/cs361/battleship/Main.java

--- src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
+++ src/main/java/edu/oregonstate/cs361/battleship/BattleshipModel.java
@@ -1,23 +1,7 @@
 package edu.oregonstate.cs361.battleship;
 
 /**
- * Created by casters on 2/1/17.
- * Holds ship objects and arrays of objects containing x,y coordinates
+ * Created by michaelhilton on 1/26/17.
  */
 public class BattleshipModel {
-    AircraftCarrier aircraftCarrier = new AircraftCarrier();
-    Battleship battleship = new Battleship();
-    Cruiser cruiser = new Cruiser();
-    Destroyer destroyer = new Destroyer();
-    Submarine submarine = new Submarine();
-    Computer_AircraftCarrier computer_aircraftCarrier = new Computer_AircraftCarrier();
-    Computer_Battleship computer_battleship = new Computer_Battleship();
-    Computer_Cruiser computer_cruiser = new Computer_Cruiser();
-    Computer_Destroyer computer_destroyer = new Computer_Destroyer();
-    Computer_Submarine computer_submarine = new Computer_Submarine();
-    //Array of start objects used for coordinates, because client side expects them in ordered paris with "Across" and "Down" keys 
-    Start[] playerHits = new Start[0];
-    Start[] playerMisses = new Start[0];
-    Start[] computerHits = new Start[0];
-    Start[] computerMisses = new Start[0];
 }
