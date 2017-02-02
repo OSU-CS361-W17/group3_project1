d2581be7de649496bc611c525a12eaccc9e4752d
Anya Lehman <lehmana@oregonstate.edu>
2017-02-02 14:03:17 -0800

    Attempt at a first commit - placeShip new stuff

--- src/main/java/edu/oregonstate/cs361/battleship/Main.java
+++ src/main/java/edu/oregonstate/cs361/battleship/Main.java
@@ -17,7 +17,9 @@ public class Main {
         post("/fire/:row/:col", (req, res) -> fireAt(req));
         //This will listen to POST requests and expects to receive a game model, as well as location to place the Ship
         post("/placeShip/:id/:row/:col/:orientation", (req, res) -> placeShip(req));
+        //placeShip(req);
     }
+
 
     //This function should return a new model
     private static String newModel() {
@@ -47,7 +49,13 @@ public class Main {
 
     //This controller should take a json object from the front End, and place the Ship as requested, and then return the object.
     private static String placeShip(Request req) {
-        string ship = req.attribute("id");
+        String shiptype = req.params(":id");
+        int row = Integer.parseInt(req.params(":row"));
+        int col = Integer.parseInt(req.params(":col"));
+        String ore = req.params(":orientation");
+
+        System.out.println(shiptype);
+        System.out.println(ore);
         return "1";
     }
 
