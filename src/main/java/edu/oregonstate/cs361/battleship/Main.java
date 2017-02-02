72f14fccf010d1a3b129448f050af9dbd26c70b5
Anya Lehman <lehmana@oregonstate.edu>
2017-02-02 15:13:12 -0800

    Merge https://github.com/OSU-CS361-W17/project1

--- src/main/java/edu/oregonstate/cs361/battleship/Main.java
+++ src/main/java/edu/oregonstate/cs361/battleship/Main.java
@@ -1,50 +1,32 @@
 package edu.oregonstate.cs361.battleship;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import spark.Request;
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.staticFiles;
+
 public class Main {
 
     public static void main(String[] args) {
         //This will allow us to server the static pages such as index.html, app.js, etc.
         staticFiles.location("/public");
+
         //This will listen to GET requests to /model and return a clean new model
         get("/model", (req, res) -> newModel());
         //This will listen to POST requests and expects to receive a game model, as well as location to fire to
         post("/fire/:row/:col", (req, res) -> fireAt(req));
-        //This will listen to POST requests and expects to receive a game model, as well as location to place the Ship
+        //This will listen to POST requests and expects to receive a game model, as well as location to place the ship
         post("/placeShip/:id/:row/:col/:orientation", (req, res) -> placeShip(req));
     }
 
     //This function should return a new model
-    private static String newModel() {
-        //creates a new board with all ships starting and ending at (0,0)
-        BattleshipModel theBoard = new BattleshipModel();
-        //only sets start and end coordinates for computer
-        theBoard.computer_aircraftCarrier.start.setStart(2,2);
-        theBoard.computer_aircraftCarrier.end.setEnd(2,7);
-        theBoard.computer_battleship.start.setStart(2,8);
-        theBoard.computer_battleship.end.setEnd(6,8);
-        theBoard.computer_cruiser.start.setStart(4,1);
-        theBoard.computer_cruiser.end.setEnd(4,4);
-        theBoard.computer_destroyer.start.setStart(7,3);
-        theBoard.computer_destroyer.end.setEnd(7,5);
-        theBoard.computer_submarine.start.setStart(9,6);
-        theBoard.computer_submarine.end.setEnd(9,8);
-        //Makes a gson obj to take in java objects
-        Gson retobj = new Gson();
-        //puts gson obj in string for return to GET
-        String retstring = retobj.toJson(theBoard);
-        return retstring;
+    static String newModel() {
+        return "MODEL";
     }
+
     //This function should accept an HTTP request and deseralize it into an actual Java object.
     private static BattleshipModel getModelFromReq(Request req){
-        Gson gson = new Gson();                                                                //creates a new Gson class variable
-        BattleshipModel battleshipmodel = gson.fromJson(req.body(), BattleshipModel.class);    //parses game model from json to a java object
-        return battleshipmodel;
+        return null;
     }
 
    private static String placeShip(Request req) {
@@ -54,7 +36,6 @@ public class Main {
         String ore = req.params(":orientation");
 
         BattleshipModel mine = getModelFromReq(req);
-        
         System.out.println(shiptype);
         System.out.println(ore);
         // test
@@ -62,6 +43,8 @@ public class Main {
     }
 
     //Similar to placeShip, but with firing.
-    private static String fireAt(Request req) { return null; }
+    private static String fireAt(Request req) {
+        return null;
+    }
 
 }
