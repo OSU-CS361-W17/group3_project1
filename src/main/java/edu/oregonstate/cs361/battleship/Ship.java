8f94970661ebc2494c3488cf5d088d9cd449fd3d
Swarles-Barkley <casters@oregonstate.edu>
2017-02-01 21:01:35 -0800

    added notes

--- src/main/java/edu/oregonstate/cs361/battleship/Ship.java
+++ src/main/java/edu/oregonstate/cs361/battleship/Ship.java
@@ -2,6 +2,8 @@ package edu.oregonstate.cs361.battleship;
 
 /**
  * Created by casters on 2/1/17.
+ * Parent class of all ships, passes on start and end coordinates that get set by newModel() or placeShip(), if they're computer or
+ * player respectively. Name and length declared in child classes.
  */
 public class Ship {
     Start start = new Start();
