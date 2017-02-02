ede28a525e26c77e702a46cde1c970c4c7073f5b
Swarles-Barkley <casters@oregonstate.edu>
2017-02-01 21:05:07 -0800

    added notes

--- src/main/java/edu/oregonstate/cs361/battleship/Start.java
+++ src/main/java/edu/oregonstate/cs361/battleship/Start.java
@@ -2,6 +2,9 @@ package edu.oregonstate.cs361.battleship;
 
 /**
  * Created by casters on 2/1/17.
+ * Holds an x,y coordinate in the form of an ordered keyed pair, x being indicated by "Across:" y being indicated by "Down".
+ * Held by Ship class and all children
+ * Also used in newModel() as coordinates for hits and misses
  */
 public class Start {
     public int Across = 0;
